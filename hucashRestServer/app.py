85aa8fef2252826ea25dba6d5e25163b178ef1d5
Ali Şahin Balıkçı <b21426696@cs.hacettepe.edu.tr>
2018-12-09 15:11:31 +0300

    Pending/Confirm URLs Added

--- hucashRestServer/app.py
+++ hucashRestServer/app.py
@@ -43,11 +43,11 @@ def createCustomer():
   #  controlRequest = requests.get("http://localhost:3000/api/Customer/"+customerId)
   #  content = controlRequest.json()
 
-    controlRequest2 = requests.get("http://localhost:3000/api/Customer")
-    content2 = controlRequest2.json()
+    requestForDefiningId = requests.get("http://localhost:3000/api/Customer")
+    content = requestForDefiningId.json()
 
-    if content2 != None or content2 != {}:
-        oldId = content2[-1]['customerId']
+    if content != None or content != {}:
+        oldId = content[-1]['customerId']
         oldIdParse = oldId.split("-")
         newIdNumber = int(oldIdParse[1]) + 1
         newId = "customer-" + str(newIdNumber) 
@@ -72,6 +72,43 @@ def createCustomer():
     r = requests.post('http://localhost:3000/api/Customer', data = jsonCustomer)
     r = requests.post('http://localhost:3000/api/CustomerWallet', data = jsonWallet)
     return redirect ("/wallet")
+
+
+#login olmus kullaniciyi direk sender olarak almamiz gerekiyor, bir de username demistik,
+#usernameden receiver id ceken bir yapi olusturmamiz gerekir
+#Ornek url http://localhost:5000/transaction/pending?senderId=customer-5&receiverId=customer-6&amount=2000
+@app.route("/transaction/pending")
+def sendHucashTxPending():
+
+    senderId = request.args['senderId']
+    receiverId = request.args['receiverId']
+    amount = request.args['amount'] 
+
+    jsonPendingTx = {
+        "$class": "org.example.basic.SendHucashTransactionPending",
+        "sender": "resource:org.example.basic.Customer#" + senderId,
+        "receiver": "resource:org.example.basic.Customer#" + receiverId,
+        "amount": amount
+    }
+
+    r = requests.post('http://localhost:3000/api/SendHucashTransactionPending', data = jsonPendingTx)
+    return redirect("/wallet")
+
+
+#protoId yi bi yerden pass etmek lazim yoksa kullanici nerden bilsin protosunu falan
+#Ornek Url http://localhost:5000/transaction/confirm?protoTxId=d9481e173f8f60194b23a4c9f0d39b9ef8714074 
+@app.route("/transaction/confirm")
+def confirmPendingTx():
+
+    prototypeId = request.args['protoTxId']
+
+    jsonConfirmTx = {
+        "$class": "org.example.basic.ConfirmTransaction",
+        "tp": "resource:org.example.basic.TransactionPrototype#" + prototypeId
+    }
+
+    r = requests.post('http://localhost:3000/api/ConfirmTransaction', data = jsonConfirmTx)
+    return redirect("/wallet")
 
 
 #ileride customerID yi otomatik cekmemiz gerekiyor, o yuzden tamamlanmis degil
