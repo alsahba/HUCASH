0327e9686e5255bae323a8c877916c095c0e6ece
Ali Şahin Balıkçı <b21426696@cs.hacettepe.edu.tr>
2018-12-09 13:51:59 +0300

    More encapsualted version of creating new customer

--- hucashRestServer/app.py
+++ hucashRestServer/app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 #random donus ekrani
 @app.route("/wallet")
 def returnScreen():
-    return "Welcome"
+    return "Islemin basarili gecti sahip"
 
 
 #customer wallet yaratabilecegin url .../createCustomerWallet?walletId=9&balance=1000 seklinde kullaniliyor.
@@ -30,40 +30,49 @@ def createEntity():
 	return redirect ("/wallet")
 
 
-#customer yaratabilecegin url .../createCustomer?customerId=cust99&firstName=bihter&lastname=ziyagil seklinde kullaniliyor.
+#customer yaratabilecegin url .../createCustomer?firstName=bihter&lastname=ziyagil seklinde kullaniliyor.
 #customer yaratirken customer walletini de otomatik yaratiyor.
+#customer id ve wallet id otomatik generate ediyor.
 @app.route("/createCustomer")
 def createCustomer():
 
-    customerId = request.args['customerId']
+    #customerId = request.args['customerId']
     firstName = request.args['firstName']
     lastName = request.args['lastName'] 
 
-    controlRequest = requests.get("http://localhost:3000/api/Customer/"+customerId)
-    content = controlRequest.json()
+  #  controlRequest = requests.get("http://localhost:3000/api/Customer/"+customerId)
+  #  content = controlRequest.json()
 
-    #control for customerId
-    try: 
-        check = content['error']
-        jsonCustomer = {
+    controlRequest2 = requests.get("http://localhost:3000/api/Customer")
+    content2 = controlRequest2.json()
+
+    if content2 != None or content2 != {}:
+        oldId = content2[-1]['customerId']
+        oldIdParse = oldId.split("-")
+        newIdNumber = int(oldIdParse[1]) + 1
+        newId = "customer-" + str(newIdNumber) 
+
+    else:
+        newId = "customer-1"
+
+    jsonCustomer = {
         "$class": "org.example.basic.Customer",
-        "customerId": customerId,
+        "customerId": newId,
         "firstName": firstName,
         "lastName": lastName,
-        "wallet": "org.example.basic.CustomerWallet#"+customerId + "Wallet"
-        }
+        "wallet": "org.example.basic.CustomerWallet#" + newId + "-wallet"
+    }
 
-        jsonWallet = {
-            "$class": "org.example.basic.CustomerWallet",
-            "walletId": customerId + "Wallet",
-            "balance": "0"
-        }
+    jsonWallet = {
+        "$class": "org.example.basic.CustomerWallet",
+        "walletId": newId + "-wallet",
+        "balance": "0"
+    }
 
-        r = requests.post('http://localhost:3000/api/Customer', data = jsonCustomer)
-        r = requests.post('http://localhost:3000/api/CustomerWallet', data = jsonWallet)
-        return redirect ("/creditCardScreen")
-    except:
-        return None
+    r = requests.post('http://localhost:3000/api/Customer', data = jsonCustomer)
+    r = requests.post('http://localhost:3000/api/CustomerWallet', data = jsonWallet)
+    return redirect ("/wallet")
+
 
 #ileride customerID yi otomatik cekmemiz gerekiyor, o yuzden tamamlanmis degil
 #hucash satin alma deneme url si normalde banka url sine baglanip ordan amountu ve customer cardindan id cekip islemin oyle yapilmasi gerekiyor bence
